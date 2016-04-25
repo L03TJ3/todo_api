dacf593759dba8b17cf57a901ff26d65f4587866
Laurence <laurenceweening@gmail.com>
2016-04-25 15:33:19 +0200

    Set up model for tasks w/ seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Task.create([
+  { title: "Create react todo app", completed: true },
+  { title: "Create a controller for tasks"},
+  { title: "Create routing"},
+  { title: "Print out some JSON"}
+])
+
+puts "Seeded #{Task.count} Tasks"
